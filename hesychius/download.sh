78037cf36ab0b1ec08be36509032d4427dda4edf
Kierán Meinhardt <kmein@posteo.de>
2022-10-06 08:16:35 +0200

    hesychius: fix long entries upstream and handle them here

--- hesychius/download.sh
+++ hesychius/download.sh
@@ -5,5 +5,5 @@ root=https://el.wikisource.org
 curl -sSL "$root/wiki/%CE%93%CE%BB%CF%8E%CF%83%CF%83%CE%B1%CE%B9" \
   | htmlq --attribute href 'ol li a[title^="Γλ"]' \
   | while read -r slug; do
-    curl -sSL "$root$slug" | htmlq dd | sed 's#</\?dd>##g;s#&lt;#<#g;s#&gt;#>#g'
+    curl -sSL "$root$slug" | htmlq dt,dd | tr -d '\n' | sed 's#\(<dt>\)#\n\1#g;s#<dt>\|</dd>##g;s#</dt><dd>#· #g;s#&lt;#<#g;s#&gt;#>#g'
   done
