e71d09ba89bb1e210a7e103aaa3fe619dbc801e7
Kierán Meinhardt <kmein@posteo.de>
2025-11-17 08:03:34 +0100

    group-expense: init

---
+++ group-expense/2025.scm
@@ -0,0 +1,8 @@
+(
+ (laura 67.65 (lisa laura johannes kieran))
+ (lisa 24 (lisa laura johannes kieran)) ; brotmuseum
+ (lisa 19.98 (lisa)) ; kaffee + tabs
+ (lisa 10.45 (laura)) ; honig + nüsse
+ (lisa 5.99 (lisa laura johannes kieran)) ; vegane würste + nüsse
+ (johannes 1.89 (lisa laura johannes kieran)) ; reeses
+)
